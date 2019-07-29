6aef072e2056f0b488823e3c9fa8d810ba28b7ed
Hui Qin Ng <huiqin.my@gmail.com>
2019-07-29 21:42:46 +0800

    Clean up code

---
+++ CAFlappyBird/CAFlappyBird/Bird.swift
@@ -0,0 +1,64 @@
+//
+//  Bird.swift
+//  CAFlappyBird
+//
+//  Created by Hui Qin Ng on 7/29/19.
+//  Copyright © 2019 Hui Qin Ng. All rights reserved.
+//
+
+import UIKit
+
+private let birdDropY: CGFloat = 2
+
+class Bird: CALayer {
+    enum State: String {
+        case up = "redbird-upflap"
+        case mid = "redbird-midflap"
+        case down = "redbird-downflap"
+    }
+
+    var state: State = .down {
+        didSet {
+            contents = UIImage(named: state.rawValue)?.cgImage
+        }
+    }
+
+    override init(layer: Any) {
+        super.init(layer: layer)
+        setupViews()
+    }
+
+    override init() {
+        super.init()
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+        contents = UIImage(named: State.mid.rawValue)?.cgImage
+        contentsGravity = .resizeAspectFill
+    }
+
+    func animate() {
+        switch state {
+        case .up: state = .mid
+        case .mid: state = .down
+        case .down: state = .up
+        }
+    }
+
+    func fly() {
+        frame.origin.y -= 80
+    }
+
+    func gravity() {
+        frame.origin.y += birdDropY
+    }
+
+    func drop() {
+        frame.origin.y = UIScreen.main.bounds.height
+    }
+}
--- CAFlappyBird/CAFlappyBird/ViewController.swift
+++ CAFlappyBird/CAFlappyBird/ViewController.swift
@@ -12,77 +12,24 @@ private let birdWidth: CGFloat = 51
 private let birdHeight: CGFloat = 36
 private let birdOriginX = floor((UIScreen.main.bounds.width - birdWidth)/2)
 private let birdOriginY = floor((UIScreen.main.bounds.height - birdHeight)/2)
-private let birdDropY: CGFloat = 2
+
 private let baseHeight = floor(UIScreen.main.bounds.width / 336 * 112)
 private let baseOriginY = UIScreen.main.bounds.height - baseHeight
 private let pipeWidth: CGFloat = 78
 private let pipeHeight: CGFloat = 480
 private let pipeHorizontalDistance: CGFloat = 180
 private let pipeVerticalDistance: CGFloat = 160
-private let pipMinHeight = UIScreen.main.bounds.height - pipeHeight - baseHeight - pipeVerticalDistance
+private let pipeMinHeight = UIScreen.main.bounds.height - pipeHeight - baseHeight - pipeVerticalDistance
 private let pipeMaxHeight = pipeHeight
-
 private let gameOverWidth: CGFloat = 192
 private let gameOverHeight: CGFloat = 42
 private let gameOverOriginX = floor((UIScreen.main.bounds.width - gameOverWidth)/2)
 private let gameOverOriginY = floor((UIScreen.main.bounds.height - baseHeight - gameOverHeight)/2)
 
 
-class Bird: CALayer {
-    enum State: String {
-        case up = "redbird-upflap"
-        case mid = "redbird-midflap"
-        case down = "redbird-downflap"
-    }
-
-    var state: State = .down {
-        didSet {
-            contents = UIImage(named: state.rawValue)?.cgImage
-        }
-    }
-
-    override init(layer: Any) {
-        super.init(layer: layer)
-        setupViews()
-    }
-
-    override init() {
-        super.init()
-        setupViews()
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    private func setupViews() {
-        contents = UIImage(named: State.mid.rawValue)?.cgImage
-        contentsGravity = .resizeAspectFill
-    }
-
-    func animate() {
-        switch state {
-        case .up: state = .mid
-        case .mid: state = .down
-        case .down: state = .up
-        }
-    }
-
-    func fly() {
-        frame.origin.y -= 80
-    }
-
-    func gravity() {
-        frame.origin.y += birdDropY
-    }
-
-    func drop() {
-        frame.origin.y = UIScreen.main.bounds.height
-    }
-}
-
 class ViewController: UIViewController {
 
+    // MARK: - Properties
     enum GameState {
         case idle
         case running
@@ -121,6 +68,8 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    // MARK: - Lifecycle
 
     deinit {
         stopTimer()
@@ -131,7 +80,9 @@ class ViewController: UIViewController {
         setupViews()
         setupGesture()
     }
-
+    
+    // MARK: - Initializers
+    
     private func setupViews() {
         view.backgroundColor = .systemTeal
 
@@ -155,7 +106,7 @@ class ViewController: UIViewController {
 
         let offset: CGFloat = UIScreen.main.bounds.width
         for index in 0..<30 {
-            let height = floor(CGFloat.random(in: pipMinHeight...pipeMaxHeight))
+            let height = floor(CGFloat.random(in: pipeMinHeight...pipeMaxHeight))
             let originX = (pipeWidth + pipeHorizontalDistance) * CGFloat(index) + offset
             let upperPipeLayer = CALayer()
             let lowerPipeLayer = CALayer()
@@ -172,47 +123,11 @@ class ViewController: UIViewController {
     }
 
     private func setupGesture() {
-            let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-            view.addGestureRecognizer(tap)
-        }
-
-    private func startTimer() {
-        timer = CADisplayLink(target: self, selector: #selector(handleNextFrameState))
-        timer?.add(to: RunLoop.current, forMode: .default)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tap)
     }
 
-    private func stopTimer() {
-        timer?.remove(from: RunLoop.current, forMode: .default)
-        timer?.invalidate()
-    }
-
-    @objc func handleNextFrameState() {
-        CATransaction.begin()
-        CATransaction.disableActions()
-        // Trigger bird flying animation
-        counter += 1
-        if counter % 6 == 0 {
-            bird.animate()
-        }
-        counter %= 6
-
-        bird.gravity()
-
-        if bird.frame.origin.y >= UIScreen.main.bounds.height - baseHeight {
-            return
-        }
-
-        pipes.forEach { pipe in
-            pipe.frame.origin.x -= 1
-            if pipe.frame.intersects(bird.frame) && (pipe.frame.intersection(bird.frame).size.width > 20 ||
-                pipe.frame.intersection(bird.frame).size.height > 20) {
-                state = .gameOver
-            }
-        }
-
-        CATransaction.commit()
-    }
-
+    // MARK: - State handler
     @objc func handleTap() {
         switch state {
         case .running:
@@ -229,5 +144,47 @@ class ViewController: UIViewController {
         }
     }
 
+}
+
+// MARK: - Timer
+private extension ViewController {
+    func startTimer() {
+        timer = CADisplayLink(target: self, selector: #selector(handleNextFrameState))
+        timer?.add(to: RunLoop.current, forMode: .default)
+    }
+
+    func stopTimer() {
+        timer?.remove(from: RunLoop.current, forMode: .default)
+        timer?.invalidate()
+        timer = nil
+    }
+
+    @objc func handleNextFrameState() {
+        CATransaction.begin()
+        CATransaction.disableActions()
+        // Trigger bird flying animation
+        counter += 1
+        if counter % 6 == 0 {
+            bird.animate()
+        }
+        counter %= 6
+
+        bird.gravity()
+
+        if bird.frame.origin.y >= UIScreen.main.bounds.height - baseHeight {
+            state = .gameOver
+            return
+        }
+
+        pipes.forEach { pipe in
+            pipe.frame.origin.x -= 1
+            if pipe.frame.intersects(bird.frame) && (pipe.frame.intersection(bird.frame).size.width > 20 ||
+                pipe.frame.intersection(bird.frame).size.height > 20) {
+                state = .gameOver
+            }
+        }
+
+        CATransaction.commit()
+    }
 }
 
